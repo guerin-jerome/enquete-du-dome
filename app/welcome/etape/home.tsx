c28c9102b5e0c7dc60a80e66657f080bbdc57afe
Jérôme GUERIN <guerin_jerome@icloud.com>
2025-06-23 23:05:06 +0200

    fix style mobile

--- app/welcome/etape/home.tsx
+++ app/welcome/etape/home.tsx
@@ -6,6 +6,7 @@ export function Home({ goLogin }: { goLogin: () => void }) {
         src="https://i.ibb.co/ksX071gk/background.png"
         alt="Bienvenue à toi enquêtrice"
       />
+      <br />
       <h1 className="typewriter-effect">Bienvenue au Dôme</h1>
       <br />
       <button onClick={goLogin}>Commencer</button>
